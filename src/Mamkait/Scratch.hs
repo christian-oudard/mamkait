a08455274f92cef4eb91803ee8190ac5ee92d17d
Christian Oudard <christian.oudard@gmail.com>
2020-12-18 11:28:38 -0800

    Remove half-done code.

--- src/Mamkait/Scratch.hs
+++ src/Mamkait/Scratch.hs
@@ -2,33 +2,3 @@ module Mamkait.Scratch where
 
 import Mamkait.Phonology
 import Mamkait.Cr
-import Mamkait.Vr
-
--- Standard Vowel-Form Sequence
---
---
-
-
-
--- Series 1 Series 2 Series 3* Series 4
--- Form 1 a ai ia / oä ao
--- Form 2 ä au iä / uä ae
--- Form 3 e ei ie / oë ea
--- Form 4 ï eu ië / uë eo
--- Form 5 i ëi ëu eë
--- Form 6 ö ou uö / iö öe
--- Form 7 o oi uo / io oe
--- Form 8 ü iu ue / eö öa
--- Form 9 u ui ua / aö oa
--- * When preceded by y-, Series 3 forms beginning with -i use their alternate forms instead (e.g., yoä, not yia), while Serie
-
--- "a", "ä", "e", "ï", "i", "ö", "o", "ü", "u",
--- "ai", "au", "ei", "eu", "ëi", "ou", "oi", "iu", "ui",
--- "ia/oä", "iä/uä", "ie/oë", "ië/uë", "ëu", "uö/iö", "uo/io", "ue/eö", "ua/aö",
--- "ao", "ae", "ea", "eo", "eë", "öe", "oe", "öa", "oa",
--- "a'a", "ä'ä", "e'e", "ï'ï", "i'i", "ö'ö", "o'o", "ü'ü", "u'u",
--- "a'i", "a'u", "e'i", "e'u", "ë'i", "o'u", "o'i", "i'u", "u'i",
--- "i'a", "i'ä", "i'e", "i'ë", "ë'u", "u'ö", "u'o", "u'e", "u'a",
--- "a'o", "a'e", "e'a", "e'o", "e'ë", "ö'e", "o'e", "ö'a", "o'a",
-
-
